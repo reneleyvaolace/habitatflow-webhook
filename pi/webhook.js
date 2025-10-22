5561f9028b1b5f2d22d2d40b37c9d5e32946518c
reneleyvaolace <125059997+reneleyvaolace@users.noreply.github.com>
2025-10-22 17:47:36 -0600

    fix: Forzar redeploy en Vercel

--- pi/webhook.js
+++ pi/webhook.js
@@ -3,3 +3,4 @@ module.exports = (req, res) => {
   // Esta es la respuesta inicial simple para verificar que Vercel funciona.
   res.status(200).send('Webhook de HabitatFlow inicializado. OK.');
 };
+// Forzar redeploy

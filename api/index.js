e40740dc3c1caa0d366f3aef8fe7a24625d3aac5
reneleyvaolace <125059997+reneleyvaolace@users.noreply.github.com>
2025-10-23 15:12:32 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-// api/index.js (Sintaxis CommonJS)
+// api/index.js
 module.exports = (req, res) => {
-  res.status(200).send('Webhook de HabitatFlow inicializado. OK. ¡Comprobado!');
+  res.status(200).send('Webhook de HabitatFlow inicializado. ¡ÉXITO FINAL!');
 };

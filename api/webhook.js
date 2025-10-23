6826f42839f452796eb4d517992ae2bfede3c9a9
reneleyvaolace <125059997+reneleyvaolace@users.noreply.github.com>
2025-10-22 18:29:42 -0600

    Delete api/webhook.js

--- api/webhook.js
+++
@@ -1,6 +0,0 @@
-// api/webhook.js
-module.exports = (req, res) => {
-  // Esta es la respuesta inicial simple para verificar que Vercel funciona.
-  res.status(200).send('Webhook de HabitatFlow inicializado. OK.');
-};
-// Forzar redeploy

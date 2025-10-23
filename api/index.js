e5b15e163213690016ced6b92a644f2f800e8bac
reneleyvaolace <125059997+reneleyvaolace@users.noreply.github.com>
2025-10-23 15:07:03 -0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-// api/index.js
-module.exports = (req, res) => {
-  res.status(200).send('Webhook de HabitatFlow inicializado. OK.');
-};
+// api/index.js (NUEVA SINTAXIS)
+
+// Esta es la función de verificación de webhook.
+export default function handler(req, res) {
+  res.status(200).send('Webhook de HabitatFlow inicializado. OK. ¡Sincronizado!');
+}
